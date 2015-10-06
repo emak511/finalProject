848949ac5ab631e7234075cc98f31d9a9678cbde
cartoon1110 <cartoon1110@hanmail.net>
2015-10-06 15:29:40 +0900

    appServlet도 수정햇듬

--- CapsuleProject/src/main/java/com/mycompany/myapp/service/MemberService.java
+++ CapsuleProject/src/main/java/com/mycompany/myapp/service/MemberService.java
@@ -1,5 +1,51 @@
 package com.mycompany.myapp.service;
 
-public class MemberService {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mycompany.myapp.dao.MemberDao;
+import com.mycompany.myapp.dto.Member;
+
+@Component
+public class MemberService {
+	@Autowired
+	private MemberDao memberDao;
+	
+	public String addMember(Member member){
+		String check=null;
+		/* 회원가입 하려는 이메일이 데이터베이스에 존재하는지 확인 */
+		Member mem=memberDao.select(member.getMemberEmail());
+		
+		/* 존재하지 않으면 중복 회원가입이 아니기 때문에 회원가입 성공 */
+		if(mem==null){
+			check="OK";
+			memberDao.insert(member);
+
+		}else{  // 존재하면 중복 회원가입이므로 회원가입 실패
+			check="Fail";
+		}
+		return check;
+	}
+	
+	public String login(String memberEmail, String memberPW, HttpSession session){
+		String state=null;
+		Member member=memberDao.select(memberEmail);
+		
+		if(member!=null){
+			if(memberEmail.equals(member.getMemberEmail())){
+				if(memberPW.equals(member.getMemberPw())){
+					session.setAttribute("memberEmail", memberEmail);
+					state="OK";
+				}else{
+					state="wrongPw";
+				}
+			}
+		}else{
+			state="wrondEmail";
+		}
+		
+		return state;
+	}
 }
