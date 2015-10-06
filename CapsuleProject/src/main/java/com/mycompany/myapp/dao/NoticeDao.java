9f6487f2b82a9f04ec0a6fe64782371accecb15f
emak511 <emak511@naver.com>
2015-10-06 15:45:45 +0900

    notice

--- CapsuleProject/src/main/java/com/mycompany/myapp/dao/NoticeDao.java
+++ CapsuleProject/src/main/java/com/mycompany/myapp/dao/NoticeDao.java
@@ -1,12 +1,17 @@
+package com.mycompany.myapp.dao;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.swing.tree.RowMapper;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import com.mycompany.myapp.dto.Notice;
@@ -14,21 +19,20 @@ import com.mycompany.myapp.dto.Notice;
 @Component
 public class NoticeDao {
 
-
 		@Autowired
 		private JdbcTemplate jdbcTemplate;
 		
 		public Integer insert(Notice notice) {
 			Integer pk = null;
-			String sql = "insert into notices(notice_date, notice_title, notice_content) values(now(),?,?)";
+			String sql = "insert into notices(notice_date, notice_title, notice_content, member_email) values(now(),?,?,?)";
 			KeyHolder keyHolder = new GeneratedKeyHolder();
 			jdbcTemplate.update(new PreparedStatementCreator() {
 				@Override
 				public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
 					PreparedStatement pstmt = conn.prepareStatement(sql, new String[] {"notice_no"});
-					pstmt.setString(1, board.getTitle());n
-					pstmt.setString(2, board.getContent());
-				
+					pstmt.setString(1, notice.getNoticeTitle());
+					pstmt.setString(2, notice.getNoticeContent());
+					pstmt.setString(3, notice.getMemberEmail());
 					return pstmt;
 				}
 			},keyHolder);
@@ -37,93 +41,69 @@ public class NoticeDao {
 			return pk;
 		}
 		
-		public List<Board> selectByPage(int pageNo, int rowsPerPage) {
+		public List<Notice> selectByPage(int noticeNo, int rowsPerPage) {
 			String sql = "";
-			sql += "select board_no, board_title, board_writer, board_date, board_hitcount " ;
-			sql += "from boards ";
-			sql += "order by board_no desc ";
+			sql += "select notice_no, notice_title, notice_date" ;
+			sql += "from notices ";
+			sql += "order by notice_no desc ";
 			sql += "limit ?,?";
 			
-			List<Board> list = jdbcTemplate.query(
+			List<Notice> list = jdbcTemplate.query(
 				sql, 
-				new Object[] { (pageNo-1)*rowsPerPage, rowsPerPage },
-				new RowMapper<Board>() {
+				new Object[] { (noticeNo-1)*rowsPerPage, rowsPerPage },
+				new RowMapper<Notice>() {
 					@Override
-					public Board mapRow(ResultSet rs, int rowNum) throws SQLException {
-						Board board = new Board();
-						board.setNo(rs.getInt("board_no"));
-						board.setTitle(rs.getString("board_title"));
-						board.setDate(rs.getDate("board_date"));
-						board.setWriter(rs.getString("board_writer"));
-						board.setHitcount(rs.getInt("board_hitcount"));
-						return board;
+					public Notice mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Notice notice = new Notice();
+						notice.setNoticeNo(rs.getInt("notice_no"));
+						notice.setNoticeTitle(rs.getString("notice_title"));
+						notice.setNoticeDate(rs.getDate("notice_date"));
+						return notice;
 					}
 				}
 			);
 			return list;
 		}
 		
-		public Board selectByPk(int boardNo) {
-			String sql = "select * from boards where board_no=?";
-			Board board = jdbcTemplate.queryForObject(
+		public Notice selectByPk(int noticeNo) {
+			String sql = "select * from notices where notice_no=?";
+			Notice notice = jdbcTemplate.queryForObject(
 				sql,
-				new Object[] {boardNo},
-				new RowMapper<Board>() {
+				new Object[] {noticeNo},
+				new RowMapper<Notice>() {
 					@Override
-					public Board mapRow(ResultSet rs, int rowNum) throws SQLException {
-						Board board = new Board();
-						board.setNo(rs.getInt("board_no"));
-						board.setTitle(rs.getString("board_title"));
-						board.setContent(rs.getString("board_content"));
-						board.setDate(rs.getDate("board_date"));
-						board.setWriter(rs.getString("board_writer"));
-						board.setHitcount(rs.getInt("board_hitcount"));  
-						board.setOriginalFileName(rs.getString("board_original_file_name"));
-						board.setFilesystemName(rs.getString("board_filesystem_name"));
-						board.setContentType(rs.getString("board_content_type"));
-						return board;
+					public Notice mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Notice notice = new Notice();
+						notice.setNoticeNo(rs.getInt("notice_no"));
+						notice.setNoticeTitle(rs.getString("notice_title"));
+						notice.setNoticeContent(rs.getString("notice_content"));
+						notice.setNoticeDate(rs.getDate("notice_date"));
+						return notice;
 					}
 				}
 			);
-			return board;
+			return notice;
 		}
 		
-		public int update(Board board) {
-			String sql = "update boards set board_title=?, board_content=? where board_no=?";
+		public int update(Notice notice) {
+			String sql = "update notices set notice_title=?, notice_content=? where notice_no=?";
 			int rows = jdbcTemplate.update(
 				sql,
-				board.getTitle(),
-				board.getContent(),
-				board.getNo()
+				notice.getNoticeTitle(),
+				notice.getNoticeContent(),
+				notice.getNoticeNo()
 			);
 			return rows;
 		}
 		
-		public int delete(long boardNo) {
-			String sql = "delete from boards where board_no=?";
+		public int delete(long noticeNo) {
+			String sql = "delete from notices where notice_no=?";
 			int rows = jdbcTemplate.update(
 				sql,
-				boardNo
+				noticeNo
 			);
 			return rows;
 		}
 		
-		public int updateHitcount(long boardNo) {
-			String sql = "update boards set board_hitcount=board_hitcount+1 where board_no=?";
-			int rows = jdbcTemplate.update(sql, boardNo);
-			return rows;
-		}	
-		
-		public int selectCount() {
-			String sql = "select count(*) from boards";
-			int rows = jdbcTemplate.queryForObject(sql, Integer.class);
-			return rows;
-		}
-	}
-
-
-
-
-}
 
 }
--- CapsuleProject/src/main/java/com/mycompany/myapp/dto/Notice.java
+++ CapsuleProject/src/main/java/com/mycompany/myapp/dto/Notice.java
@@ -3,10 +3,24 @@ package com.mycompany.myapp.dto;
 import java.util.*;
 
 public class Notice {
-
+	private int noticeNo;
 	private Date noticeDate;
 	private String noticeTitle;
 	private String noticeContent;
+	private String memberEmail;
+	
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public int getNoticeNo() {
+		return noticeNo;
+	}
+	public void setNoticeNo(int noticeNo) {
+		this.noticeNo = noticeNo;
+	}
 	
 	public Date getNoticeDate() {
 		return noticeDate;
