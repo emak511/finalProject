1f3528d4508890c5ab6dc35c3959496054c3e390
emak511 <emak511@naver.com>
2015-10-09 13:26:37 +0900

    ff

--- CapsuleProject/src/main/java/com/mycompany/myapp/controller/DiaryController.java
+++ CapsuleProject/src/main/java/com/mycompany/myapp/controller/DiaryController.java
@@ -1,5 +1,6 @@
 package com.mycompany.myapp.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,6 @@ import com.mycompany.myapp.service.DiaryService;
 @Controller
 public class DiaryController {
 	
-	@Autowired
-	private DiaryService diaryService;
 	
 	@RequestMapping("/diary/diary")
 	public String diary(){
